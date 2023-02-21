58c9b15b8529e102e48426b188b4870216c56d5b
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2023-02-21 21:28:50 +0530

    Update what-type-of-triangle.sql

--- what-type-of-triangle.sql
+++ what-type-of-triangle.sql
@@ -1,4 +1,4 @@
-SELECT CASE             
+/*SELECT CASE             
             WHEN A + B > C AND B + C > A AND A + C > B THEN
                 CASE 
                     WHEN A = B AND B = C THEN 'Equilateral'
@@ -7,4 +7,12 @@ SELECT CASE
                 END
             ELSE 'Not A Triangle'
         END
-FROM TRIANGLES;
+FROM TRIANGLES;*/
+
+select case
+when a+b<=c or b+c<=a or c+a<=b then "Not A Triangle"
+when a=b and b=c then "Equilateral"
+when a=b or b=c or a=c then "Isosceles"
+when a!=b and b!= c then "Scalene"
+end
+from Triangles;
